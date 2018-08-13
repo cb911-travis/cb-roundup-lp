a4f5f091e112708786812e4b8393ff0a5d18ff5c
Travis Wagner <twagner@globaltrading.local>
2018-08-13 09:36:14 -0400

    added sticky nav

--- modules/nav/scripts.js
+++ modules/nav/scripts.js
@@ -1,1 +1,24 @@
-console.log('nav');
+// sticky nav on scroll
+$(document).ready(function () {
+  var nav = $('.nav').offset();
+
+  if ($(window).scrollTop() >= nav.top) {
+    $("body").addClass("stuck");
+  } else {
+    $("body").removeClass("stuck")
+  }
+
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= nav.top) {
+      $("body").addClass("stuck");
+    } else {
+      $("body").removeClass("stuck")
+    }
+  });
+
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    offset: 50
+  });
+
+});
