dcdb3f0638328a3623b72908032b3e55dcd51e94
Travis Wagner <twagner@globaltrading.local>
2018-08-14 09:48:16 -0400

    toggle name menu, etc

--- modules/nav/scripts.js
+++ modules/nav/scripts.js
@@ -1,11 +1,13 @@
-// sticky nav on scroll
-$(document).ready(function () {
+/**
+ * Sticky Nav on Scroll
+ */
+(function () {
   var nav = $('.nav').offset();
 
   if ($(window).scrollTop() >= nav.top) {
     $("body").addClass("stuck");
   } else {
-    $("body").removeClass("stuck")
+    $("body").removeClass("stuck");
   }
 
 
@@ -13,12 +15,21 @@ $(document).ready(function () {
     if ($(window).scrollTop() >= nav.top) {
       $("body").addClass("stuck");
     } else {
-      $("body").removeClass("stuck")
+      $("body").removeClass("stuck");
     }
   });
 
-  var scroll = new SmoothScroll('a[href*="#"]', {
-    offset: 50
-  });
+//  var scroll = new SmoothScroll('a[href*="#"]', {
+//    offset: 50
+//  });
 
+})();
+
+$('a[href^="#"]').click(function(e){
+  e.preventDefault();
+});
+
+// toggle menu on click
+$('.js-nav__toggle').click(function () {
+  $('.nav__menu').toggleClass('is-active');
 });
